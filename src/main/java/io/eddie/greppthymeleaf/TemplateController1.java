4cbaa2687bf18eea9eec2578f1bd410c3e5d611e
eddie1031 <archaco123@gmail.com>
2025-04-02 14:12:51 +0900

    ~타임리프 문법

--- src/main/java/io/eddie/greppthymeleaf/TemplateController1.java
+++ src/main/java/io/eddie/greppthymeleaf/TemplateController1.java
@@ -1,7 +1,10 @@
 package io.eddie.greppthymeleaf;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class TemplateController1 {
@@ -11,6 +14,46 @@ public class TemplateController1 {
         return "index";
     }
 
+    @GetMapping("/page1")
+    public String showPage1(Model model) {
+        model.addAttribute("msg", "대체된 메세지!");
+        return "page1";
+    }
+
+    @GetMapping("/page2")
+    public String showPage2(
+            Article article,
+            Model model
+    ) {
+        model.addAttribute("article", article);
+        return "page2";
+    }
+
+    @Data
+    public class Article {
+        private int id;
+        private String title;
+    }
+
+    @GetMapping("/page3")
+    public String showPage3(Model model) {
+
+        String unsafeString = "<script>alert('메롱');</script>";
+
+        model.addAttribute("script",
+                unsafeString);
+
+        return "page3";
+    }
+
+    @GetMapping("/page4")
+    public String showPage4(Model model) {
+
+        model.addAttribute("cellPhone", "j11");
+        model.addAttribute("countryCallingCode", "+82");
+
+        return "page4";
+    }
 
 
 
