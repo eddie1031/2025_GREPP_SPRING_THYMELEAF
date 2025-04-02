9150358b4ebb28985b362ea7c1f5448d0084711d
eddie1031 <archaco123@gmail.com>
2025-04-02 10:26:53 +0900

    1. 인덱스

---
+++ src/main/java/io/eddie/greppthymeleaf/TemplateController1.java
@@ -0,0 +1,17 @@
+package io.eddie.greppthymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TemplateController1 {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+
+
+
+}
---
+++ src/test/java/io/eddie/greppthymeleaf/GreppThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eddie.greppthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreppThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
